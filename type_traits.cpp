d952ec1d55112b5f5bea621da04575b5f9e7b2a5
brano-san <branoverv10@bk.ru>
2023-09-16 23:52:24 +0300

    Created is_same and HasBeginEnd traits

---
+++ type_traits.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+using namespace std;
+
+template<typename T>
+class HasBeginEnd
+{
+private:
+    template<class U>
+    static constexpr auto cheker(U* u) -> decltype((*u).begin(), (*u).end(), bool()) { return true; }
+
+    template<class>
+    static constexpr auto cheker(...) { return false; }
+
+public:
+    static bool constexpr value = cheker<T>(nullptr);
+};
+
+template<typename T>
+class HasConstBeginEnd
+{
+private:
+    template<class U>
+    static constexpr auto constCheker(U* u) -> decltype((*u).cbegin(), (*u).cend(), bool()) { return true; }
+
+    template<class>
+    static constexpr auto constCheker(...) { return false; }
+
+public:
+    static bool constexpr value = constCheker<T>(nullptr);
+};
+
+template<typename T, typename U>
+class IsSame
+{
+public:
+    static bool constexpr value = false;
+};
+
+template<typename T>
+class IsSame<T, T>
+{
+public:
+    static bool constexpr value = true;
+};
+
+class MyClass
+{
+public:
+
+};
+
+int main()
+{
+    std::is_same<int, float>::value_type;
+    cout << HasBeginEnd<vector<int>>::value << endl;
+    cout << HasBeginEnd<list<int>>::value << endl;
+    cout << HasBeginEnd<MyClass>::value << endl;
+}
