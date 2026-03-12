c020ac2d282eca63209d31881b1402893d4e29d0
brano-san <branoverv10@bk.ru>
2026-03-12 23:22:30 +0300

    Fix errors in cv. Add tests for all traits

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,45 @@
 
 int main()
 {
+    static_assert(std::is_same_v<traits::RemoveReferenceT<int*>, int*>);
+    static_assert(std::is_same_v<traits::RemoveReferenceT<int>, int>);
+    static_assert(std::is_same_v<traits::RemoveReferenceT<int&>, int>);
+    static_assert(std::is_same_v<traits::RemoveReferenceT<int&&>, int>);
+
+    static_assert(std::is_same_v<traits::RemovePointerT<int*>, int>);
+    static_assert(std::is_same_v<traits::RemovePointerT<const int*>, const int>);
+    static_assert(std::is_same_v<traits::RemovePointerT<volatile int*>, volatile int>);
+    static_assert(std::is_same_v<traits::RemovePointerT<const volatile int*>, const volatile int>);
+    static_assert(std::is_same_v<traits::RemovePointerT<const volatile int&>, const volatile int&>);
+
+    static_assert(std::is_same_v<traits::RemoveConstT<int>, int>);
+    static_assert(std::is_same_v<traits::RemoveConstT<const int>, int>);
+    static_assert(std::is_same_v<traits::RemoveConstT<const volatile int>, volatile int>);
+    static_assert(std::is_same_v<traits::RemoveConstT<const volatile int*>, const volatile int*>);
+    static_assert(std::is_same_v<traits::RemoveConstT<volatile int* const>, volatile int*>);
+    static_assert(std::is_same_v<traits::RemoveConstT<const volatile int&>, const volatile int&>);
+
+    static_assert(std::is_same_v<traits::RemoveCvT<int>, int>);
+    static_assert(std::is_same_v<traits::RemoveCvT<const int>, int>);
+    static_assert(std::is_same_v<traits::RemoveCvT<const volatile int>, int>);
+    static_assert(std::is_same_v<traits::RemoveCvT<const volatile int*>, const volatile int*>);
+    static_assert(std::is_same_v<traits::RemoveCvT<volatile int* const>, volatile int*>);
+    static_assert(std::is_same_v<traits::RemoveCvT<int* volatile const>, int*>);
+    static_assert(std::is_same_v<traits::RemoveCvT<const volatile int&>, const volatile int&>);
+
+    ////////////////////////
+
+    static_assert(traits::IsSameV<int, int>);
+    static_assert(!traits::IsSameV<int, double>);
+    static_assert(!traits::IsSameV<float, double>);
+    static_assert(!traits::IsSameV<int, int*>);
+
+    static_assert(traits::IsVoidV<void>);
+    static_assert(!traits::IsVoidV<int*>);
+
+    static_assert(traits::IsNullptrV<nullptr_t>);
+    static_assert(!traits::IsNullptrV<int*>);
+
     static_assert(!traits::IsPointerV<int>);
     static_assert(traits::IsPointerV<int*>);
 
--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -123,6 +123,9 @@ template <typename T>
 struct RemoveReference<T&&>: Remove<T>
 {};
 
+template <typename T>
+using RemoveReferenceT = typename RemoveReference<T>::type;
+
 // RemovePointer
 template <typename T>
 struct RemovePointer: Remove<T>
@@ -131,6 +134,9 @@ struct RemovePointer: Remove<T>
 template <typename T>
 struct RemovePointer<T*>: Remove<T>
 {};
+
+template <typename T>
+using RemovePointerT = typename RemovePointer<T>::type;
 
 // RemoveConst
 template <typename T>
@@ -141,6 +147,9 @@ template <typename T>
 struct RemoveConst<const T>: Remove<T>
 {};
 
+template <typename T>
+using RemoveConstT = typename RemoveConst<T>::type;
+
 // RemoveVolatile
 template <typename T>
 struct RemoveVolatile: Remove<T>
@@ -150,46 +159,45 @@ template <typename T>
 struct RemoveVolatile<volatile T>: Remove<T>
 {};
 
+template <typename T>
+using RemoveVolatileT = typename RemoveVolatile<T>::type;
+
 // RemoveCV
 template <typename T>
-struct RemoveCv: Remove<T>
-{};
+struct RemoveCv
+{
+    using type = typename RemoveConst<typename RemoveVolatile<T>::type>::type;
+};
 
 template <typename T>
-struct RemoveCv<const T>: Remove<T>
-{};
-
-template <typename T>
-struct RemoveCv<volatile T>: Remove<T>
-{};
-
-template <typename T>
-struct RemoveCv<const volatile T>: Remove<T>
-{};
+using RemoveCvT = typename RemoveCv<T>::type;
 
 // Is* functions
 // IsSame
 template <typename T, typename U>
-struct IsSame
-{
-    static constexpr bool value = false;
-};
-
-template <typename T>
-struct IsSame<T, T>
-{
-    static constexpr bool value = true;
-};
-
-// is void
-template <typename T>
-struct IsVoid: IsSame<void, RemoveCv<T>>
+struct IsSame: std::false_type
 {};
 
-// is nullptr
 template <typename T>
-struct IsNullptr: IsSame<std::nullptr_t, RemoveCv<T>>
+struct IsSame<T, T>: std::true_type
 {};
+
+template <typename T, typename U>
+inline constexpr bool IsSameV = IsSame<T, U>::value;
+
+template <typename T>
+struct IsVoid: IsSame<void, typename RemoveCv<T>::type>
+{};
+
+template <typename T>
+inline constexpr bool IsVoidV = IsVoid<T>::value;
+
+template <typename T>
+struct IsNullptr: IsSame<std::nullptr_t, typename RemoveCv<T>::type>
+{};
+
+template <typename T>
+inline constexpr bool IsNullptrV = IsNullptr<T>::value;
 
 template <typename T>
 struct IsPointer: std::false_type
