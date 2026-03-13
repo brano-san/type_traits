d3078c8a77da96839f906e681d29f68074ce48e1
brano-san <branoverv10@bk.ru>
2026-03-13 21:23:09 +0300

    To std style

--- src/main.cpp
+++ src/main.cpp
@@ -36,61 +36,61 @@ public:
 
 int main()
 {
-    static_assert(std::is_same_v<traits::RemoveReferenceT<int*>, int*>);
-    static_assert(std::is_same_v<traits::RemoveReferenceT<int>, int>);
-    static_assert(std::is_same_v<traits::RemoveReferenceT<int&>, int>);
-    static_assert(std::is_same_v<traits::RemoveReferenceT<int&&>, int>);
+    static_assert(std::is_same_v<traits::remove_reference_t<int*>, int*>);
+    static_assert(std::is_same_v<traits::remove_reference_t<int>, int>);
+    static_assert(std::is_same_v<traits::remove_reference_t<int&>, int>);
+    static_assert(std::is_same_v<traits::remove_reference_t<int&&>, int>);
 
-    static_assert(std::is_same_v<traits::RemovePointerT<int*>, int>);
-    static_assert(std::is_same_v<traits::RemovePointerT<const int*>, const int>);
-    static_assert(std::is_same_v<traits::RemovePointerT<volatile int*>, volatile int>);
-    static_assert(std::is_same_v<traits::RemovePointerT<const volatile int*>, const volatile int>);
-    static_assert(std::is_same_v<traits::RemovePointerT<const volatile int&>, const volatile int&>);
+    static_assert(std::is_same_v<traits::remove_pointer_t<int*>, int>);
+    static_assert(std::is_same_v<traits::remove_pointer_t<const int*>, const int>);
+    static_assert(std::is_same_v<traits::remove_pointer_t<volatile int*>, volatile int>);
+    static_assert(std::is_same_v<traits::remove_pointer_t<const volatile int*>, const volatile int>);
+    static_assert(std::is_same_v<traits::remove_pointer_t<const volatile int&>, const volatile int&>);
 
-    static_assert(std::is_same_v<traits::RemoveConstT<int>, int>);
-    static_assert(std::is_same_v<traits::RemoveConstT<const int>, int>);
-    static_assert(std::is_same_v<traits::RemoveConstT<const volatile int>, volatile int>);
-    static_assert(std::is_same_v<traits::RemoveConstT<const volatile int*>, const volatile int*>);
-    static_assert(std::is_same_v<traits::RemoveConstT<volatile int* const>, volatile int*>);
-    static_assert(std::is_same_v<traits::RemoveConstT<const volatile int&>, const volatile int&>);
+    static_assert(std::is_same_v<traits::remove_const_t<int>, int>);
+    static_assert(std::is_same_v<traits::remove_const_t<const int>, int>);
+    static_assert(std::is_same_v<traits::remove_const_t<const volatile int>, volatile int>);
+    static_assert(std::is_same_v<traits::remove_const_t<const volatile int*>, const volatile int*>);
+    static_assert(std::is_same_v<traits::remove_const_t<volatile int* const>, volatile int*>);
+    static_assert(std::is_same_v<traits::remove_const_t<const volatile int&>, const volatile int&>);
 
-    static_assert(std::is_same_v<traits::RemoveCvT<int>, int>);
-    static_assert(std::is_same_v<traits::RemoveCvT<const int>, int>);
-    static_assert(std::is_same_v<traits::RemoveCvT<const volatile int>, int>);
-    static_assert(std::is_same_v<traits::RemoveCvT<const volatile int*>, const volatile int*>);
-    static_assert(std::is_same_v<traits::RemoveCvT<volatile int* const>, volatile int*>);
-    static_assert(std::is_same_v<traits::RemoveCvT<int* volatile const>, int*>);
-    static_assert(std::is_same_v<traits::RemoveCvT<const volatile int&>, const volatile int&>);
+    static_assert(std::is_same_v<traits::remove_cv_t<int>, int>);
+    static_assert(std::is_same_v<traits::remove_cv_t<const int>, int>);
+    static_assert(std::is_same_v<traits::remove_cv_t<const volatile int>, int>);
+    static_assert(std::is_same_v<traits::remove_cv_t<const volatile int*>, const volatile int*>);
+    static_assert(std::is_same_v<traits::remove_cv_t<volatile int* const>, volatile int*>);
+    static_assert(std::is_same_v<traits::remove_cv_t<int* volatile const>, int*>);
+    static_assert(std::is_same_v<traits::remove_cv_t<const volatile int&>, const volatile int&>);
 
     ////////////////////////
 
-    static_assert(traits::IsSameV<int, int>);
-    static_assert(!traits::IsSameV<int, double>);
-    static_assert(!traits::IsSameV<float, double>);
-    static_assert(!traits::IsSameV<int, int*>);
+    static_assert(traits::is_same_v<int, int>);
+    static_assert(!traits::is_same_v<int, double>);
+    static_assert(!traits::is_same_v<float, double>);
+    static_assert(!traits::is_same_v<int, int*>);
 
-    static_assert(traits::IsVoidV<void>);
-    static_assert(!traits::IsVoidV<int*>);
+    static_assert(traits::is_void_v<void>);
+    static_assert(!traits::is_void_v<int*>);
 
-    static_assert(traits::IsNullptrV<nullptr_t>);
-    static_assert(!traits::IsNullptrV<int*>);
+    static_assert(traits::is_nullptr_v<nullptr_t>);
+    static_assert(!traits::is_nullptr_v<int*>);
 
-    static_assert(!traits::IsPointerV<int>);
-    static_assert(traits::IsPointerV<int*>);
+    static_assert(!traits::is_pointer_v<int>);
+    static_assert(traits::is_pointer_v<int*>);
 
-    static_assert(!traits::IsReferenceV<int>);
-    static_assert(!traits::IsReferenceV<int*>);
-    static_assert(traits::IsReferenceV<int&>);
-    static_assert(traits::IsReferenceV<int&&>);
-    static_assert(traits::IsReferenceV<const int&&>);
-    static_assert(traits::IsReferenceV<const volatile int&&>);
+    static_assert(!traits::is_reference_v<int>);
+    static_assert(!traits::is_reference_v<int*>);
+    static_assert(traits::is_reference_v<int&>);
+    static_assert(traits::is_reference_v<int&&>);
+    static_assert(traits::is_reference_v<const int&&>);
+    static_assert(traits::is_reference_v<const volatile int&&>);
 
-    static_assert(!traits::IsConstV<int>);
-    static_assert(!traits::IsConstV<volatile int>);
-    static_assert(!traits::IsConstV<const volatile int*>);
-    static_assert(traits::IsConstV<const volatile int>);
-    static_assert(traits::IsConstV<volatile int* const>);
-    static_assert(traits::IsConstV<int* volatile const>);
+    static_assert(!traits::is_const_v<int>);
+    static_assert(!traits::is_const_v<volatile int>);
+    static_assert(!traits::is_const_v<const volatile int*>);
+    static_assert(traits::is_const_v<const volatile int>);
+    static_assert(traits::is_const_v<volatile int* const>);
+    static_assert(traits::is_const_v<int* volatile const>);
 
     ////////////////////////
 
--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -4,135 +4,144 @@
 
 namespace traits {
 
-namespace details {
 template <typename T>
-struct Remove
+struct remove_reference
 {
     using type = T;
 };
-}  // namespace details
 
 template <typename T>
-struct RemoveReference: details::Remove<T>
-{};
-
-template <typename T>
-struct RemoveReference<T&>: details::Remove<T>
-{};
-
-template <typename T>
-struct RemoveReference<T&&>: details::Remove<T>
-{};
-
-template <typename T>
-using RemoveReferenceT = typename RemoveReference<T>::type;
-
-template <typename T>
-struct RemovePointer: details::Remove<T>
-{};
-
-template <typename T>
-struct RemovePointer<T*>: details::Remove<T>
-{};
-
-template <typename T>
-using RemovePointerT = typename RemovePointer<T>::type;
-
-template <typename T>
-struct RemoveConst: details::Remove<T>
-{};
-
-template <typename T>
-struct RemoveConst<const T>: details::Remove<T>
-{};
-
-template <typename T>
-using RemoveConstT = typename RemoveConst<T>::type;
-
-template <typename T>
-struct RemoveVolatile: details::Remove<T>
-{};
-
-template <typename T>
-struct RemoveVolatile<volatile T>: details::Remove<T>
-{};
-
-template <typename T>
-using RemoveVolatileT = typename RemoveVolatile<T>::type;
-
-template <typename T>
-struct RemoveCv
+struct remove_reference<T&>
 {
-    using type = typename RemoveConst<typename RemoveVolatile<T>::type>::type;
+    using type = T;
 };
 
 template <typename T>
-using RemoveCvT = typename RemoveCv<T>::type;
+struct remove_reference<T&&>
+{
+    using type = T;
+};
+
+template <typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+template <typename T>
+struct remove_pointer
+{
+    using type = T;
+};
+
+template <typename T>
+struct remove_pointer<T*>
+{
+    using type = T;
+};
+
+template <typename T>
+using remove_pointer_t = typename remove_pointer<T>::type;
+
+template <typename T>
+struct remove_const
+{
+    using type = T;
+};
+
+template <typename T>
+struct remove_const<const T>
+{
+    using type = T;
+};
+
+template <typename T>
+using remove_const_t = typename remove_const<T>::type;
+
+template <typename T>
+struct remove_volatile
+{
+    using type = T;
+};
+
+template <typename T>
+struct remove_volatile<volatile T>
+{
+    using type = T;
+};
+
+template <typename T>
+using remove_volatile_t = typename remove_volatile<T>::type;
+
+template <typename T>
+struct remove_cv
+{
+    using type = typename remove_const<typename remove_volatile<T>::type>::type;
+};
+
+template <typename T>
+using remove_cv_t = typename remove_cv<T>::type;
 
 template <typename T, typename U>
-struct IsSame: std::false_type
+struct is_same: std::false_type
 {};
 
 template <typename T>
-struct IsSame<T, T>: std::true_type
+struct is_same<T, T>: std::true_type
 {};
 
 template <typename T, typename U>
-inline constexpr bool IsSameV = IsSame<T, U>::value;
+inline constexpr bool is_same_v = is_same<T, U>::value;
 
 template <typename T>
-struct IsVoid: IsSame<void, typename RemoveCv<T>::type>
+struct is_void: is_same<void, typename remove_cv<T>::type>
 {};
 
 template <typename T>
-inline constexpr bool IsVoidV = IsVoid<T>::value;
+inline constexpr bool is_void_v = is_void<T>::value;
 
 template <typename T>
-struct IsNullptr: IsSame<std::nullptr_t, typename RemoveCv<T>::type>
+struct is_nullptr: is_same<std::nullptr_t, typename remove_cv<T>::type>
 {};
 
 template <typename T>
-inline constexpr bool IsNullptrV = IsNullptr<T>::value;
+inline constexpr bool is_nullptr_v = is_nullptr<T>::value;
 
 template <typename T>
-struct IsPointer: std::false_type
+struct is_pointer: std::false_type
 {};
 
 template <typename T>
-struct IsPointer<T*>: std::true_type
+struct is_pointer<T*>: std::true_type
 {};
 
 template <typename T>
-inline constexpr bool IsPointerV = IsPointer<T>::value;
+inline constexpr bool is_pointer_v = is_pointer<T>::value;
 
 template <typename T>
-struct IsReference: std::false_type
+struct is_reference: std::false_type
 {};
 
 template <typename T>
-struct IsReference<T&>: std::true_type
+struct is_reference<T&>: std::true_type
 {};
 
 template <typename T>
-struct IsReference<T&&>: std::true_type
+struct is_reference<T&&>: std::true_type
 {};
 
 template <typename T>
-inline constexpr bool IsReferenceV = IsReference<T>::value;
+inline constexpr bool is_reference_v = is_reference<T>::value;
 
 template <typename T>
-struct IsConst: std::false_type
+struct is_const: std::false_type
 {};
 
 template <typename T>
-struct IsConst<const T>: std::true_type
+struct is_const<const T>: std::true_type
 {};
 
 template <typename T>
-inline constexpr bool IsConstV = IsConst<T>::value;
+inline constexpr bool is_const_v = is_const<T>::value;
 
 namespace imagine {
-
 template <typename T, typename = void>
 struct has_member_foo: std::false_type
 {};
@@ -143,6 +152,6 @@ struct has_member_foo<T, std::void_t<decltype(T::foo)>>: std::true_type
 
 template <typename T>
 inline constexpr bool has_member_foo_v = has_member_foo<T>::value;
-
 }  // namespace imagine
+
 }  // namespace traits
